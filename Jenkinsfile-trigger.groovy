0e5e8e3394ecfb180bb3fdc05e64e05bea004c7a
konda24srikanth <konda.srikanth@walkingtree.tech>
2024-04-16 15:23:46 +0530

    updated

--- Jenkinsfile-trigger.groovy
+++ Jenkinsfile-trigger.groovy
@@ -8,27 +8,25 @@ pipeline {
                      subject: "Approval Needed: Execute Job",
                      body: "Please approve the execution of the job by clicking the following link: ${env.BUILD_URL}input/"
 
-
                 timeout(time: 30, unit: 'MINUTES') {
-
                     script {
                         def userInput = input(
                             id: 'userInput',
                             message: 'Do you approve the execution of this job?',
-                            parameters: [
+                            parameters: [                     
                                 [
-                                    $class: 'BooleanParameterDefinition',
-                                    defaultValue: false,
-                                    description: 'Approve?',
-                                    name: 'APPROVE_JOB'
+                                    $class: 'SubmitterParameterDefinition', // Use SubmitterParameter for user-specific approval
+                                    name: 'APPROVE_JOB',
+                                    submitter: 'admin', // Specify the Jenkins User ID for approval
+                                    defaultValue: 'Proceed', // Default value for the approval button
+                                    description: 'Approve this job execution' // Description of the approval parameter
                                 ]
                             ]
                         )
 
-
-                        if (userInput) {
-                            echo "Job approved by user."
-
+                        // Check the user input
+                        if (userInput == 'Proceed') {
+                            echo "Job approved by user: admin"
                         } else {
                             echo "Job not approved. Exiting the pipeline."
                             currentBuild.result = 'ABORTED'
