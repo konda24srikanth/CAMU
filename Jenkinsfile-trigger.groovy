554d3e75a3293cd52665d8b3e07b0ac01610a410
konda24srikanth <konda.srikanth@walkingtree.tech>
2024-04-16 15:12:20 +0530

    updated

--- Jenkinsfile-trigger.groovy
+++ Jenkinsfile-trigger.groovy
@@ -2,38 +2,41 @@ pipeline {
     agent any
 
     stages {
-    stage('Trigger Job After Approval') {
+        stage('Trigger Job After Approval') {
             steps {
-                mail to: 'konda.srikanth@walkingtree.tech',
+                // Send approval request email to designated approver
+                mail to: 'ko@walkingtree.tech',
                      subject: "Approval Needed: Execute Job",
                      body: "Please approve the execution of the job by clicking the following link: ${env.BUILD_URL}input/"
 
-
+                // Wait for approval
                 timeout(time: 30, unit: 'MINUTES') {
-
-                    script {
-                        def userInput = input(
-                            id: 'userInput',
-                            message: 'Do you approve the execution of this job?',
-                            parameters: [
-                                [
-                                    $class: 'BooleanParameterDefinition',
-                                    defaultValue: false,
-                                    description: 'Approve?',
-                                    name: 'APPROVE_JOB'
-                                ]
+                    // Wait for user input
+                    def userInput = input(
+                        id: 'userInput',
+                        message: 'Do you approve the execution of this job?',
+                        parameters: [
+                            [
+                                $class: 'BooleanParameterDefinition',
+                                defaultValue: false,
+                                description: 'Approve?',
+                                name: 'APPROVE_JOB'
                             ]
-                        )
+                        ]
+                    )
 
+                    // Validate user approval by checking the username of the user who approved
+                    def approvedBy = env.APPROVED_BY ?: 'unknown'
 
-                        if (userInput) {
-                            echo "Job approved by user."
-
-                        } else {
-                            echo "Job not approved. Exiting the pipeline."
-                            currentBuild.result = 'ABORTED'
-                            error("Job execution aborted by user.")
-                        }
+                    if (userInput && approvedBy == 'admin') {
+                        echo "Job approved by user 'srikanth'. Proceeding with job execution..."
+                        // Add job execution steps here
+                        // For example:
+                        sh 'echo "Executing job..."'
+                    } else {
+                        echo "Job not approved by user 'srikanth'. Exiting the pipeline."
+                        currentBuild.result = 'ABORTED'
+                        error("Job execution aborted or unauthorized.")
                     }
                 }
             }
